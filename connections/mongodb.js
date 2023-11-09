1b357434f18f9374149432fb5433043d9bdc011b
Sreehari Nallapaneni <45209359+nallapaneni-sreehari@users.noreply.github.com>
2023-11-09 14:56:19 +0530

    Update mongodb.js

--- connections/mongodb.js
+++ connections/mongodb.js
@@ -3,8 +3,10 @@ const mongoose = require('mongoose');
 async function connect()
 {
     const connectionUrl = process.env.MONGODB_URI;
+    const connectionUrl2 = "mongodb+srv://r151149:sree123@cluster0.ecexy.mongodb.net/track-the-mail-db?retryWrites=true&w=majority";
 
-    mongoose.connect(connectionUrl, {useNewUrlParser:true, useUnifiedTopology:true}, (err)=>{
+    console.log('connectionUrl  :: ', connectionUrl);
+    mongoose.connect(connectionUrl2, {useNewUrlParser:true, useUnifiedTopology:true}, (err)=>{
         if(err) throw err;
         console.log("Connected to Database");
     });
